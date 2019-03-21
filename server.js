68146644a7778bfa45e4c30d580fd96da97cf98d
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-03-21 19:58:03 +0300

    get rid of hard-coded URl

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const PORT = process.env.PORT || 5000;
 
 var express = require("express");
- 
+var mustacheExpress = require('mustache-express');
+
 var app = express();
+
+//Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 //make way for some custom css, js and images
@@ -11,6 +17,10 @@ app.use('/fonts', express.static(__dirname + '/public/fonts'));
 app.use('/js', express.static(__dirname + '/public/js'));
 
 app.set('GATEWAY_SERVER_URL', process.env.GATEWAY_SERVER_URL);
+
+app.get('/index.html', function(req, res){
+	  res.render('index.html', {GATEWAY_SERVER_URL: process.env.GATEWAY_SERVER_URL});
+});
 
 var server = app.listen(PORT, function(){
     var port = server.address().port;
