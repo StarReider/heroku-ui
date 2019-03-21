6bcc344a5e634dd18922e37a5000d24b9a700048
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-03-21 20:30:36 +0300

    get rid of hrad coded URL

--- server.js
+++ server.js
@@ -6,20 +6,18 @@ var mustacheExpress = require('mustache-express');
 var app = express();
 
 //Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public');
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/fonts', express.static(__dirname + '/public/fonts'));
 app.use('/js', express.static(__dirname + '/public/js'));
 
-app.set('GATEWAY_SERVER_URL', process.env.GATEWAY_SERVER_URL);
-
 app.get('/index.html', function(req, res){
-	  res.render('index.html', {GATEWAY_SERVER_URL: process.env.GATEWAY_SERVER_URL});
+	  res.render('index.html', {"GATEWAY_SERVER_URL": process.env.GATEWAY_SERVER_URL + ""});
 });
 
 var server = app.listen(PORT, function(){
