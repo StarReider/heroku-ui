a492ec3a2add81a1ce504d05670815c5e887eebc
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-03-22 12:47:03 +0300

    rollback external URL feature

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const PORT = process.env.PORT || 5000;
 
 var express = require("express");
-var mustacheExpress = require('mustache-express');
-
 var app = express();
 
-//Register '.mustache' extension with The Mustache Express
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/public');
-
 app.use(express.static('public'));
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/fonts', express.static(__dirname + '/public/fonts'));
-app.use('/js', express.static(__dirname + '/public/js'));
-
-app.get('/index.html', function(req, res){
-	  res.render('index.html', {"GATEWAY_SERVER_URL": process.env.GATEWAY_SERVER_URL + ""});
-});
 
 var server = app.listen(PORT, function(){
     var port = server.address().port;
