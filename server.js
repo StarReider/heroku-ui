bc5426c0f2f588fa64f3d964771e9f6b8af3ba9b
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-04-11 19:15:08 +0300

    disable CORS for node js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const PORT = process.env.PORT || 5001;
 
 var express = require("express");
 var app = express();
-var cors = require('cors');
-
-app.use(cors());
 
 app.use(express.static('public'));
 
