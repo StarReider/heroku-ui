f4ba19fa5452fc6273e03a9498ac80e718e17a48
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-04-11 17:59:17 +0300

    https support

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ const PORT = process.env.PORT || 5001;
 
 var express = require("express");
 var app = express();
+var cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	  next();
-});
-
-var server = app.listen(PORT, function() {
+var server = app.listen(PORT, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
