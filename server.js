919b3afca96b6a403e01fa474ebc51e9e36491b5
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-04-11 19:19:26 +0300

    enable cors for node js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const PORT = process.env.PORT || 5001;
 
 var express = require("express");
 var app = express();
+var cors = require('cors');
 
+app.use(cors());
 app.use(express.static('public'));
 
 var server = app.listen(PORT, function(){
